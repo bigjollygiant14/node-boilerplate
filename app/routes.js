6e8c5e31bfe05d01f19fed9658b5c07d43fb2ce7
wjay <wjay@deloitte.com>
2016-06-09 13:38:26 -0600

    fixed serving issue

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get('*', function(req, res) {
-    var indexPath = path.join(__dirname, '../public', 'index.html')
+    var indexPath = path.join(__dirname, '../public', req.url);
     res.sendFile( indexPath );
   });
 };
