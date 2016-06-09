6f4ceff0eea5d43d4e1e656ac6a119f6a6f68d45
wjay <wjay@deloitte.com>
2016-06-09 13:36:47 -0600

    added bower, angular, bootstrap and fixed serve issue

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,8 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get('*', function(req, res) {
-    var indexPath = path.join(__dirname, '../public', 'index.html')
+    //console.log(req.url);
+    var indexPath = path.join(__dirname, '../public', req.url);
     res.sendFile( indexPath );
   });
 };
