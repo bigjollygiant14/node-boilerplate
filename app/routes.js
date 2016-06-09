76a3d713ec1a7b46c0f53997e48991cb3a8c8ace
wjay <wjay@deloitte.com>
2016-06-09 13:37:07 -0600

    removed a comment

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,6 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get('*', function(req, res) {
-    //console.log(req.url);
     var indexPath = path.join(__dirname, '../public', req.url);
     res.sendFile( indexPath );
   });
